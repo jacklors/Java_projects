22f29939eb0d316d43326dd53194ccc3be912bc5
Jackson Loredo <95225193+jacklors@users.noreply.github.com>
2022-02-17 18:50:23 -0300

    Atualizando as pastas

---
+++ Projects/Java_calc/CalculoDiferencaIdade.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class CalculoDiferencaIdade {
+	public static void main(String[] args) {
+
+		short idadeA = 0;
+		short idadeB = 0;
+		int dif = 0;
+
+
+
+
+
+
+		
+	}
+
+
+}
---
+++ Projects/Java_teste/README.md
@@ -0,0 +1,2 @@
+# Java_teste
+Testando algumas funções
